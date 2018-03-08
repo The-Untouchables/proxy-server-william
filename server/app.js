3a3dfcdfaa0d24ef8fbc040f4f049a551c2307e1
William Trousdale <william@Williams-MacBook-Air.local>
2018-03-08 11:36:26 -0800

    Uses deployed services

--- server/app.js
+++ server/app.js
@@ -6,15 +6,15 @@ app.use(express.static(__dirname + '/../public'));
 app.use('/rooms/:roomid/', express.static(__dirname + '/../public'));
 
 app.get('/rooms/:roomid/ratings', (req, res) => {
-  res.redirect('http://localhost:3004/rooms/' + req.params.roomid + '/ratings');
+  res.redirect('http://18.219.35.229:80/rooms/' + req.params.roomid + '/ratings');
 });
 
 app.get('/rooms/:roomid/reviews', (req, res) => {
-  res.redirect('http://localhost:3004/rooms/' + req.params.roomid + '/reviews');
+  res.redirect('http://18.219.35.229:80/rooms/' + req.params.roomid + '/reviews');
 });
 
 app.get('/api/rooms/:roomid/description', (req, res) => {
-  res.redirect('http://localhost:3002/rooms/' + req.params.roomid + '/description');
+  res.redirect('http://18.222.4.195:80/api/rooms/' + req.params.roomid + '/description');
 });
 
 module.exports = app;
